8a44ebef7a33caede78760c3fcaeddad8abca715
Thiago de Carvalho <thiagovowclash@gmail.com>
2024-01-26 22:58:27 -0300

    arrumei

--- config/plugins.js
+++ config/plugins.js
@@ -5,4 +5,8 @@ module.exports = ({ env }) => ({
     providerOptions: {
       cloud_name: env('CLOUDINARY_NAME'),
       api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECR
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
+  },
+  // ...
+});
