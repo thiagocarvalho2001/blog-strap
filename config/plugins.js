82e089d631463a77ada2727e89055e5974528778
Thiago de Carvalho <thiagovowclash@gmail.com>
2024-01-26 22:47:46 -0300

    blog-strapi

--- config/plugins.js
+++ config/plugins.js
@@ -5,8 +5,4 @@ module.exports = ({ env }) => ({
     providerOptions: {
       cloud_name: env('CLOUDINARY_NAME'),
       api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
-    },
-  },
-  // ...
-});
+      api_secret: env('CLOUDINARY_SECR
