5e5fc2e1083230274a2e30b6095ff96fe08b7b3f
Thiago de Carvalho <thiagovowclash@gmail.com>
2024-01-26 22:47:30 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '84f42541ca87ce05c9890fc110e3ab23'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c8fc72e0-0e5e-43e2-ae38-0a64bebb2b8e'
+};
